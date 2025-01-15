d3b827904385372a7b5256b42e7c90d43776b949
Andrew Berisha <andrew.berisha@sdggroup.com>
2025-01-15 10:55:56 -0600

    add more parsing functions
    Signed-off-by: Andrew Berisha <andrew.berisha@sdggroup.com>

--- src/parse_doc.clj
+++ src/parse_doc.clj
@@ -1,4 +1,14 @@
-(ns parse-doc)
+(ns parse-doc
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [java-time.api :as jt]))
 
-(defn run [opts]
-  (println "Hello world!"))
+(defn read-json-from-file [filename]
+  (let [json-string (slurp (io/file filename))]
+    (json/read-str json-string :key-fn keyword)))
+
+(defn parse-date-keyword [keyword]
+  (let [date-str (name keyword)
+        date (jt/local-date "MMM d, yyyy" date-str)]
+    (jt/format :iso-date date)))
+
