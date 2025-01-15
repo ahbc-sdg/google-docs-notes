28b284188aaa7b7f252cb30f973918bd4b96c17a
Andrew Berisha <andrew.berisha@sdggroup.com>
2025-01-15 12:14:59 -0600

    auto-commit [2025-01-15 Wed 12:14]

--- src/parse_doc.clj
+++ src/parse_doc.clj
@@ -12,3 +12,19 @@
         date (jt/local-date "MMM d, yyyy" date-str)]
     (jt/format :iso-date date)))
 
+(defn valid-date-keyword? [keyword]
+  (try
+    (jt/local-date "MMM d, yyyy" (name keyword))
+    true
+    (catch Exception _ false)))
+
+(defn replace-date-keys [input-map]
+  (reduce-kv
+    (fn [acc k v]
+      (if (and (instance? clojure.lang.Keyword k)
+               (valid-date-keyword? k))
+        (assoc acc (keyword (parse-date-keyword k)) v)
+        (assoc acc k v)))
+    {}
+    input-map))
+
