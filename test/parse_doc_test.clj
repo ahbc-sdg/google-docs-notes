1eb76dafc9d464d87c56ee1b7b7198486f4fa614
Andrew Berisha <andrew.berisha@sdggroup.com>
2025-01-16 10:06:23 -0600

    add new tests
    Signed-off-by: Andrew Berisha <andrew.berisha@sdggroup.com>

--- test/parse_doc_test.clj
+++ test/parse_doc_test.clj
@@ -8,4 +8,17 @@
   (is (= (parse-date-keyword (keyword "Dec 31, 2023")) "2023-12-31"))
   (is (= (parse-date-keyword (keyword "Jan 1, 2024")) "2024-01-01")))
 
+(deftest test-valid-date-keyword
+  (is (= (valid-date-keyword? (keyword "Sep 5, 2023")) true))
+  (is (= (valid-date-keyword? :nope) false)))
+
+(deftest test-replace-date-keys
+  (let [test-map {(keyword "Sep 5, 2023") "Event A"
+                  (keyword "Not a Date") "Event B"
+                  (keyword "Oct 1, 2023") "Event C"}]
+    (is (= (replace-date-keys test-map)
+           {:2023-09-05 "Event A"
+            (keyword "Not a Date") "Event B"
+            :2023-10-01 "Event C"}))))
+
 (run-tests)
