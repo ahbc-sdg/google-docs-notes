5af01a201b612a77dd84e35366686e99d9712eb0
Andrew Berisha <andrew.berisha@sdggroup.com>
2025-01-15 22:18:04 -0600

    auto-commit [2025-01-15 Wed 22:18]

--- src/parse_doc.clj
+++ src/parse_doc.clj
@@ -20,11 +20,38 @@
 
 (defn replace-date-keys [input-map]
   (reduce-kv
-    (fn [acc k v]
-      (if (and (instance? clojure.lang.Keyword k)
-               (valid-date-keyword? k))
-        (assoc acc (keyword (parse-date-keyword k)) v)
-        (assoc acc k v)))
-    {}
-    input-map))
+   (fn [acc k v]
+     (if (and (instance? clojure.lang.Keyword k)
+              (valid-date-keyword? k))
+       (assoc acc (keyword (parse-date-keyword k)) v)
+       (assoc acc k v)))
+   {}
+   input-map))
 
+(defn reshape-data [input-map]
+  (letfn
+      [(process-entry [date-key entries parent-index category acc]
+         (reduce
+          (fn [acc entry]
+            (let [parent (first entry)
+                  children (rest entry)
+                  new-index (count acc)]
+              (let [parent-entry
+                    {:index         new-index
+                     :value         parent
+                     :parent-index  parent-index
+                     :date          date-key
+                     :category      category}]
+
+                (let [updated-acc (conj acc parent-entry)]
+                  (if (seq children)
+                    (process-entry date-key (map vector children) new-index category updated-acc)
+                    updated-acc)))))
+          acc entries))]
+    (reduce-kv
+     (fn [acc category-key category-val]
+       (reduce-kv
+        (fn [acc date-key entries]
+          (process-entry date-key entries nil (name category-key) acc))
+        acc category-val))
+     [] input-map)))
